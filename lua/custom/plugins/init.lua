7131241ee651e2bd5d96ca40fc87bd364cbd6068
umar <mdumarkhan.1207@gmail.com>
2025-03-11 03:52:51 +0530

    add plugin: neo-tree, copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,22 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    {
+        'github/copilot.vim',
+        config = function()
+            vim.cmd 'Copilot enable' -- Enables Copilot on startup
+        end,
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = function()
+          require('toggleterm').setup {
+            open_mapping = [[<C-\>]], -- Open with Ctrl + \
+            direction = "float",       -- Floating terminal
+            shade_terminals = true
+          }
+        end
+      }
+}
